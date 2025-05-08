04544ba6b37cd64d8c7c732f391866e3fbbb4852
Vu Ngoc Anh <hoathong99@gmail.com>
2025-05-08 14:49:22 +0700

    update prefix url

--- Middle/src/main.ts
+++ Middle/src/main.ts
@@ -33,6 +33,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.setGlobalPrefix('api');
 
   const config = new DocumentBuilder()
     .setTitle('Cats example')
